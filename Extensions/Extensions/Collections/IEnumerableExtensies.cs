204f5dedc11d13d049c2b9e5d0b4a10b5349cb9e
barld <bakeds@gmail.com>
2014-10-04 23:05:38 +0200

    first commit

---
+++ Extensions/Extensions/Collections/IEnumerableExtensies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.Collections
+{
+    public static class IEnumerableExtensies
+    {
+        public static IEnumerable<T> Add<T>(this IEnumerable<T> nummerable, T value)
+        {
+            return nummerable.Concat(new[] { value });          
+        }
+
+        public static void test()
+        {
+            new int[1].Add(5);
+        }
+    }
+}
